b309dd81a18e773499a29f86c9af10bbbb31c4b1
Fasih Muhammad <82199241+fasihmuhammadvirk@users.noreply.github.com>
2023-08-11 16:54:59 +0500

    formatted

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express')
-var app = express()
+const express = require("express");
+var app = express();
 
 //Route
-app.get('/',function(req,res){
-  res.send('hello world')
-})
+app.get("/", function (req, res) {
+	res.send("hello world");
+});
 
 //MongoDB connection
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-mongoose.connection.once('open',function(){
-  console.log('Database connected Successfully');
-}).on('error',function(err){
-  console.log('Error', err);
-})
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
+mongoose.connection.once("open", function () {
+		console.log("Database connected Successfully");
+	}).on("error", function (err) {
+		console.log("Error", err);
+	});
 
 //Server
-app.listen(8000,function(){
-  console.log('Server is Up at: http://localhost:8000')
-})
+app.listen(8000, function () {
+	console.log("Server is Up at: http://localhost:8000");
+});
 
-const router = require('./routes/emp')
-app.use('/emp',router)                    
+const router = require("./routes/emp");
+app.use("/emp", router);
